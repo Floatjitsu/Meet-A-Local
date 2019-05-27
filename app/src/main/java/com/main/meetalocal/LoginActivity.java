94d0b6743b3053f1bdd5fd2af5ee08f02daa6be6
Timur Celik <timur.celik@hs-augsburg.de>
2019-05-27 18:39:25 +0200

    added sign up activity

---
+++ app/src/main/java/com/main/meetalocal/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.main.meetalocal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    /*
+    Start SignUpActivity Activity
+     */
+    public void onSignUp(View view) {
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+}
--- app/src/main/java/com/main/meetalocal/SignUpActivity.java
+++ app/src/main/java/com/main/meetalocal/SignUpActivity.java
@@ -3,11 +3,11 @@ package com.main.meetalocal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
 }
