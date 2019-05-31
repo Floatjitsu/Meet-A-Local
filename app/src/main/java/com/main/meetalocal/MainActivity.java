4797b23d0b20dd628f511e4217369f898a7c46d0
Timur Celik <timur.celik@hs-augsburg.de>
2019-05-31 14:48:55 +0200

    created toolbar in main activity and a menu for the nav drawer

--- app/src/main/java/com/main/meetalocal/MainActivity.java
+++ app/src/main/java/com/main/meetalocal/MainActivity.java
@@ -3,17 +3,21 @@ package com.main.meetalocal;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.main.meetalocal.viewmodel.ViewModelUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
     TextView mUserEmail, mUserFirstName;
 
     @Override
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
         mUserEmail = findViewById(R.id.test_user_email);
         mUserFirstName = findViewById(R.id.test_user_first_name);
+        mToolbar = findViewById(R.id.toolbar_main_activity);
+
+        setUpToolbar();
+
         /*
 
         ViewModelUser viewModelUser = ViewModelProviders.of(this).get(ViewModelUser.class);
@@ -37,5 +45,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }); */
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        }
     }
 }
